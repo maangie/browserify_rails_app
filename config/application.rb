3cbb8a28b7ddc81eb549be092d3b1a9cf49cb940
maangie <maangie906@gmail.com>
2015-11-11 14:59:59 +0900

    configure browserify.

--- config/application.rb
+++ config/application.rb
@@ -37,5 +37,8 @@ module BrowserifyRailsApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.browserify_rails.commandline_options =
+      "-t coffeeify --extension=\".coffee\""
   end
 end
