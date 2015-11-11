af10e71274ded6dcb03eeaecca32e0aee6f44822
maangie <maangie906@gmail.com>
2015-11-11 14:54:35 +0900

    cosme.

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,14 @@
 require File.expand_path('../boot', __FILE__)
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "sprockets/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'sprockets/railtie'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -16,6 +16,7 @@ require "sprockets/railtie"
 Bundler.require(*Rails.groups)
 
 module BrowserifyRailsApp
+  # アプリ設定
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those
     # specified here.  Application configuration should go into files
